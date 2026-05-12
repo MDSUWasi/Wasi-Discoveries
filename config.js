cad324faa2639d60d7d6db3ed7a2ee660ce20421
Md. Shafi Un Wasi <october.shafiunwasi@gmail.com>
2026-05-12 20:40:46 +0600

    cource code-1.0.0.0
    First time uploading the source code.

---
+++ config.js
@@ -0,0 +1,17 @@
+/* 
+   CONFIGURATION FOR WASI-RESEARCH
+   1. Create article as an HTML file inside the 'articles' folder.
+   2. Add the file path and details below.
+*/
+
+const articlesDB = [
+    {
+        id: "No-Cloning Theorem",
+        title: "The Universe's Copy-Paste Error: Understanding No-Cloning.",
+        category: "QUANTUM PHYSICS ",
+        date: "May 12, 2026",
+        description: "No-Cloning theorem is the natures own copy protection. Quantum states cannot be copied.",
+        file: "articles/No-Copy Theorem.html" 
+    },
+
+];
